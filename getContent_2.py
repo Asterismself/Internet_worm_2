8d42a33607269cf5f8b4cd35bf0e3ce92cf4f1f1
GYH <1751325276@qq.com>
2022-10-21 22:50:03 +0800

    兴趣开放实验室复试爬虫题第2小题第一次提交

--- getContent_2.py
+++ getContent_2.py
@@ -40,8 +40,8 @@ import datetime
 def create_request(_uid, _page):
     url = 'https://api.bilibili.com/x/relation/followings?vmid=' + str(_uid) + '&pn=' + str(page)
     headers = {
-        # "cookie": "buvid3=DF7BCC77-F113-86A9-6C37-2310EEAEBB4780865infoc; b_nut=1662784580; i-wanna-go-back=-1; _uuid=44C6FEF10-8B85-FBD6-9B110-A7246F5BEB2681905infoc; buvid4=82FF1AFC-44CC-68A8-E2F9-B79B0F97A2F682266-022091012-jFX7KwtKp+QVX3iDk1I5aA%3D%3D; nostalgia_conf=-1; rpdid=|(J~RYlm|k)R0J'uYYk)YmYJu; fingerprint=de0ee24a730b7d39587bfe60f35a977c; buvid_fp_plain=undefined; DedeUserID=649750767; DedeUserID__ckMd5=3d599dbcc55a4918; buvid_fp=c7776846d9554a43f15c41e713ef15e3; b_ut=5; PVID=1; CURRENT_QUALITY=0; LIVE_BUVID=AUTO7516658860516766; SESSDATA=77ae0619%2C1681484402%2C5e84b%2Aa1; bili_jct=f6e7833a4a2a965b5b7be6a0407671ee; sid=6xydd0d0; b_lsid=BE8518310_183E3545CA0; bp_video_offset_649750767=717626135549050900; innersign=1; CURRENT_FNVAL=4048",
-        # "referer": "https://space.bilibili.com/11632773/fans/follow",
+        "cookie": "buvid3=DF7BCC77-F113-86A9-6C37-2310EEAEBB4780865infoc; b_nut=1662784580; i-wanna-go-back=-1; _uuid=44C6FEF10-8B85-FBD6-9B110-A7246F5BEB2681905infoc; buvid4=82FF1AFC-44CC-68A8-E2F9-B79B0F97A2F682266-022091012-jFX7KwtKp+QVX3iDk1I5aA%3D%3D; nostalgia_conf=-1; rpdid=|(J~RYlm|k)R0J'uYYk)YmYJu; fingerprint=de0ee24a730b7d39587bfe60f35a977c; buvid_fp_plain=undefined; DedeUserID=649750767; DedeUserID__ckMd5=3d599dbcc55a4918; buvid_fp=c7776846d9554a43f15c41e713ef15e3; b_ut=5; PVID=1; CURRENT_QUALITY=0; LIVE_BUVID=AUTO7516658860516766; SESSDATA=77ae0619%2C1681484402%2C5e84b%2Aa1; bili_jct=f6e7833a4a2a965b5b7be6a0407671ee; sid=6xydd0d0; b_lsid=BE8518310_183E3545CA0; bp_video_offset_649750767=717626135549050900; innersign=1; CURRENT_FNVAL=4048",
+        "referer": "https://space.bilibili.com/11632773/fans/follow",
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
     }
     _request = urllib.request.Request(url=url, headers=headers)
